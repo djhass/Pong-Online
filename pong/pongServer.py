0bbbd948434b18e43e888a20fec96592b75f1f7e
JRuss2021 <jackson.russell@uky.edu>
2025-11-23 15:25:01 -0500

    Added name and reviewed
    Added my name along with my groups that were already there, and began reviewing so I may begin on the bonuses.

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -1,6 +1,6 @@
 # =================================================================================================
-# Contributing Authors:	    Daniel Hasselwander, Donnie Tkachenko
-# Email Addresses:          djha269@uky.edu, dmtk222@uky.edu
+# Contributing Authors:	    Daniel Hasselwander, Donnie Tkachenko, Jackson Russell
+# Email Addresses:          djha269@uky.edu, dmtk222@uky.edu, jgru225@uky.edu
 # Date:                     11/21/25
 # Purpose:                  The pong server host, handles client connections and relays game state
 # Misc:                     
